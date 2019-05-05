77bf85b441fac61220b44e9941bd8c5f0d13533b
GuptaReenu <reenu.cse2016@gmail.com>
2019-05-05 15:12:57 +0530

    adding existing files

---
+++ src/com/java/nnn.java
@@ -0,0 +1,24 @@
+package com.java;
+
+public class nnn
+{
+	public static void main(String args[])
+	{
+		bb ob = new bb();
+		System.out.println(ob.a);
+		bb ob1 = new bb();
+		System.out.println(ob1.a);
+		
+	}
+
+}
+class bb
+{
+	int a=2;
+	bb()
+	{
+		
+		
+	}
+	
+}
